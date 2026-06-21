3375c0948c944da5c482881669c719a272688bee
Bochit0 <jhosuabustilloscalderonq@gmail.com>
2026-06-21 19:28:02 -0400

    feat: add personalized colors and typografy

--- app/src/main/java/org/scesi/cappuchinoawesome/ui/theme/Type.kt
+++ app/src/main/java/org/scesi/cappuchinoawesome/ui/theme/Type.kt
@@ -1,34 +1,65 @@
 package org.scesi.cappuchinoawesome.ui.theme
 
+
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import androidx.compose.ui.text.font.Font
+import org.scesi.cappuchinoawesome.R
 
 // Set of Material typography styles to start with
-val Typography = Typography(
-    bodyLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 16.sp,
-        lineHeight = 24.sp,
-        letterSpacing = 0.5.sp
-    )
-    /* Other default text styles to override
-    titleLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 22.sp,
-        lineHeight = 28.sp,
-        letterSpacing = 0.sp
-    ),
-    labelSmall = TextStyle(
-        fontFamily = FontFamily.Default,
+val Typography = Typography()
+val jaquardaregular = FontFamily(
+    Font(R.font.jaquardaregular)
+)
+
+val orbitron = FontFamily(
+    Font(R.font.orbitron)
+)
+
+val Typography.subtitle: TextStyle
+    get() = TextStyle(
+        fontFamily = FontFamily.SansSerif,
         fontWeight = FontWeight.Medium,
-        fontSize = 11.sp,
+        fontSize = 18.sp,
         lineHeight = 16.sp,
         letterSpacing = 0.5.sp
     )
-    */
-)
+
+val Typography.title: TextStyle
+    get() = TextStyle(
+        fontFamily = FontFamily.Monospace,
+        fontWeight = FontWeight.Normal,
+        fontSize = 25.sp,
+        lineHeight = 28.sp,
+        letterSpacing = 0.sp
+    )
+
+val Typography.text: TextStyle
+    get() = TextStyle(
+        fontFamily = FontFamily.Serif,
+        fontWeight = FontWeight.Normal,
+        fontSize = 12.sp,
+        lineHeight = 14.sp,
+        letterSpacing = 0.sp
+    )
+
+val Typography.titleApp: TextStyle
+    get() = TextStyle(
+        fontFamily = jaquardaregular,
+        fontWeight = FontWeight.Normal,
+        fontSize = 62.sp,
+        lineHeight = 29.sp,
+        letterSpacing = 0.sp
+    )
+
+val Typography.subtitleApp: TextStyle
+    get() = TextStyle(
+        fontFamily = orbitron,
+        fontWeight = FontWeight.Normal,
+        fontSize = 45.sp,
+        lineHeight = 22.sp,
+        letterSpacing = 0.sp
+    )
