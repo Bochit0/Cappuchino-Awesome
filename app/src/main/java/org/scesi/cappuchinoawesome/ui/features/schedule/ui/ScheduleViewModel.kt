9264ad14c624144681a99daca977d807a4cf5ef5
Bochit0 <jhosuabustilloscalderonq@gmail.com>
2026-06-26 00:30:37 -0400

    change: delete parameters for use default in itemcard

---
+++ app/src/main/java/org/scesi/cappuchinoawesome/ui/features/schedule/ui/ScheduleViewModel.kt
@@ -0,0 +1,4 @@
+package org.scesi.cappuchinoawesome.ui.features.schedule.ui
+
+class ScheduleModelView {
+}
--- app/src/main/java/org/scesi/cappuchinoawesome/ui/utils/itemcard/ItemCard.kt
+++ app/src/main/java/org/scesi/cappuchinoawesome/ui/utils/itemcard/ItemCard.kt
@@ -1,54 +1,39 @@
 package org.scesi.cappuchinoawesome.ui.utils.itemcard
 
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxHeight
+import android.R.attr.textStyle
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.Card
-import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.text.TextStyle
-import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
+import org.scesi.cappuchinoawesome.ui.theme.subtitle
 
 @Composable
 fun ItemCard(
     name: String,
     onClick: () -> Unit,
     modifier: Modifier = Modifier,
-    height: Dp = 54.dp,
-    containerColor: Color = MaterialTheme.colorScheme.secondary,
-    contentColor: Color = MaterialTheme.colorScheme.background,
-    textStyle: TextStyle = MaterialTheme.typography.bodyLarge,
-    startIndent: Dp = 0.dp
 ) {
-    Card(
-        onClick = onClick,
+    Row(
         modifier = modifier
             .fillMaxWidth()
-            .padding(start = startIndent, bottom = 4.dp)
-            .height(height),
-        shape = RoundedCornerShape(6.dp),
-        colors = CardDefaults.cardColors(
-            containerColor = containerColor,
-            contentColor = contentColor
+            .height(54.dp)
+            .clickable { onClick() }
+            .padding(horizontal = 16.dp),
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.SpaceBetween
+    ){
+        Text(
+            text = name,
+            style = MaterialTheme.typography.subtitle,
+            color = MaterialTheme.colorScheme.background
         )
-    ) {
-        Box(
-            modifier = Modifier
-                .fillMaxWidth()
-                .fillMaxHeight()
-                .padding(horizontal = 16.dp),
-            contentAlignment = Alignment.CenterStart
-        ) {
-            Text(text = name, style = textStyle)
-        }
     }
 }
