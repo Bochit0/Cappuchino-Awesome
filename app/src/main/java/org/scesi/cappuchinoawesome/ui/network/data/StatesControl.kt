d5d82792b07478b8754251a81dac32947ddc9a66
Bochit0 <jhosuabustilloscalderonq@gmail.com>
2026-06-26 20:48:41 -0400

    feat: add states control for HTTP response for list of careers

--- app/src/main/java/org/scesi/cappuchinoawesome/ui/network/data/StatesControl.kt
+++ app/src/main/java/org/scesi/cappuchinoawesome/ui/network/data/StatesControl.kt
@@ -1,4 +1,8 @@
 package org.scesi.cappuchinoawesome.ui.network.data
 
-class StatesControl {
+sealed class StatesControl {
+    data object Loading : StatesControl()
+    data class Success(val carreers: List<Career>) : StatesControl()
+    data object Empty : StatesControl()
+    data class Error(val message: String) : StatesControl()
 }
