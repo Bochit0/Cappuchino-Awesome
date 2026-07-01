041bb13faa33e3e68cbd7c96cd0eb251e8c4e9d8
Bochit0 <jhosuabustilloscalderonq@gmail.com>
2026-07-01 03:10:27 -0400

    feat: add shape and crossfade to icons render

--- app/src/main/java/org/scesi/cappuchinoawesome/ui/utils/button/ButtonComponent.kt
+++ app/src/main/java/org/scesi/cappuchinoawesome/ui/utils/button/ButtonComponent.kt
@@ -1,8 +1,10 @@
 package org.scesi.cappuchinoawesome.ui.utils.button
 
+import androidx.compose.animation.Crossfade
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Icon
@@ -29,14 +31,17 @@ fun ButtonComponent(
     Button(
         onClick = onClick,
         modifier = modifier,
-        colors = colorButton
+        colors = colorButton,
+        shape = RoundedCornerShape(4.dp)
     ) {
-        if (isIcon != null) {
-            Icon(
-                imageVector = isIcon,
-                contentDescription = null,
-                modifier = Modifier.size(18.dp)
-            )
+        Crossfade(targetState = isIcon, label = "ButtonIconCrossfade") { icon ->
+            if (icon != null) {
+                Icon(
+                    imageVector = icon,
+                    contentDescription = null,
+                    modifier = Modifier.size(25.dp)
+                )
+            }
         }
 
         if (isIcon != null && isText != null) {
