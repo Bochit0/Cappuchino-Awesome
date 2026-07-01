75ace931a0850cc8a72434d8cfb814f849194253
Bochit0 <jhosuabustilloscalderonq@gmail.com>
2026-07-01 16:22:49 -0400

    feat: add info card component for about me screen

---
+++ app/src/main/java/org/scesi/cappuchinoawesome/ui/features/aboutme/AboutMeScreen.kt
@@ -0,0 +1,2 @@
+package org.scesi.cappuchinoawesome.ui.features.aboutme
+
---
+++ app/src/main/java/org/scesi/cappuchinoawesome/ui/utils/infocard/InfoCard.kt
@@ -0,0 +1,60 @@
+package org.scesi.cappuchinoawesome.ui.utils.infocard
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import org.scesi.cappuchinoawesome.ui.theme.text
+import org.scesi.cappuchinoawesome.ui.utils.icons.Icon
+
+@Composable
+fun InfoCard(
+    modifier: Modifier = Modifier,
+    title: String,
+    content: @Composable (()-> Unit)
+){
+    Box(
+        modifier = modifier.fillMaxWidth(),
+        contentAlignment = Alignment.TopCenter
+    ) {
+        Card(
+            modifier = Modifier
+                .fillMaxWidth(),
+            shape = RoundedCornerShape(4.dp),
+            colors = CardDefaults.cardColors(
+                containerColor = MaterialTheme.colorScheme.primary
+            )
+        ) {
+            Column(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(16.dp)
+            ) {
+                Text(
+                    text = title,
+                    style = MaterialTheme.typography.text,
+                    color = MaterialTheme.colorScheme.tertiary
+                )
+                HorizontalDivider(
+                    modifier = Modifier.padding(vertical = 8.dp),
+                    thickness = 3.dp,
+                    color = MaterialTheme.colorScheme.tertiary.copy(alpha = 0.2f)
+                )
+                content()
+            }
+        }
+    }
+}
